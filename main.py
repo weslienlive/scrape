f63a3fc451fbb5aa581f14ca73c926a095dd8ca4
weslienlive <weslienlive@gmail.com>
2023-03-06 18:41:02 +0200

    check other path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess
 
-chrome_path = subprocess.check_output(['which', 'google-chrome-stable']).decode().strip()
-print("Chrome path is:", chrome_path)
+bin_contents = subprocess.check_output(['ls', '/root/.nix-profile/bin']).decode().strip().split('\n')
+print("Contents of /root/.nix-profile/bin:")
+for item in bin_contents:
+    print(item)
