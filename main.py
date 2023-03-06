0c78723cb11037212fe8a3b98aa43f99cfb28708
weslienlive <weslienlive@gmail.com>
2023-03-06 18:49:52 +0200

    check browser

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import subprocess
 
-bin_contents = subprocess.check_output(['ls', '/root/.nix-profile/bin']).decode().strip().split('\n')
-print("Contents of /root/.nix-profile/bin:")
-for item in bin_contents:
-    print(item)
+# Check if Google Chrome is installed
+try:
+    subprocess.check_output(['which', 'google-chrome-stable'])
+except subprocess.CalledProcessError:
+    print("Google Chrome is not installed")
+    exit()
+
+# Launch Google Chrome
+subprocess.Popen(['google-chrome-stable'])
+
+# Get the version of Google Chrome
+version_info = subprocess.check_output(['google-chrome-stable', '--version']).decode().strip()
+print("Google Chrome version:", version_info.split()[-1])
