8d3ec5509542c90fd5a8602c596f09b0f5946d6c
weslienlive <weslienlive@gmail.com>
2023-03-08 13:54:34 +0200

    no sandbox

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ from bs4 import BeautifulSoup
 
 options = Options()
 options.add_argument('headless')
-options.add_argument('--no-sandbox')
+#options.add_argument('--no-sandbox')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
 chrome_driver_path = "/usr/local/bin/chromedriver" 
 driver = webdriver.Chrome(service=Service(executable_path=chrome_driver_path), options=options)
 driver.implicitly_wait(60)
 url = "https://www.forexfactory.com/calendar"
-print(url)
 driver.get(url)
 
 
