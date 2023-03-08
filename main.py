2d6c5cf98b8d628541ab469a43a6644b14328f71
weslienlive <weslienlive@gmail.com>
2023-03-08 14:28:08 +0200

    init ver

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ options = Options()
 options.add_argument('headless')
 #options.add_argument('--no-sandbox')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager.install()), options=options)
 url = "https://www.forexfactory.com/calendar"
 driver.get(url)
 
