e86940f13e394e52ae8c57acb87a0916c3119064
weslienlive <weslienlive@gmail.com>
2023-03-08 21:03:50 +0200

    all info

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 options = Options()
 options.add_argument('--headless')
@@ -22,37 +23,20 @@ driver.get(url)
 try:
     # Wait for the tbody tag to appear on the webpage
     tbody_locator = (By.TAG_NAME, 'tbody')
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located(tbody_locator))
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located(tbody_locator))
 
     # Get the page source and parse it with BeautifulSoup
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     # Find the table with class "calendar__table"
     calendar_table = soup.find('table', {'class': 'calendar__table'})
-    print(bool(calendar_table))
 
     # Find all the tr tags in the table
     tr_tags = calendar_table.find_all('tr')
-    print(bool(tr_tags))
-    print(len(tr_tags))
-
-
-    #print(tbody)
-except Exception as e:
-    print(f"Error: {e}")
-    print("Could not find the tbody tag on the webpage.")
-
-driver.quit()
-
-
-'''
-
-
 
     # Loop through the tr tags and only print the ones that meet the criteria
-    for tr in tr_tags:
+    for tr in tqdm(tr_tags, desc="Processing events"):
         td_tags = tr.find_all('td')
-        print(bool(td_tags))
         for td in td_tags:
             if td.text.strip() == 'US' and 'calendar__cell calendar__impact impact calendar__impact calendar__impact--low' in td['class']:
                 # Get text from <span> inside <td> with class="calendar__cell"
@@ -70,5 +54,20 @@ driver.quit()
                 print("Event Name:", event_name)
 
                 break
+
+
+    #print(tbody)
+except Exception as e:
+    print(f"Error: {e}")
+    print("Could not find the tbody tag on the webpage.")
+
+driver.quit()
+
+
+'''
+
+
+
+    
     
 '''
