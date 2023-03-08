a0e730446307a705317faa6164fb5c79de187077
weslienlive <weslienlive@gmail.com>
2023-03-08 13:06:10 +0200

    new changes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 options = Options()
 options.add_argument('headless')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+print(webdriver.chrome.driver)
 driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()), options=options)
 url = "https://www.forexfactory.com/calendar"
 driver.get(url)
