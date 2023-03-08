04ee86f40f31af4033d01fbdefea82d788572861
weslienlive <weslienlive@gmail.com>
2023-03-08 10:13:54 +0200

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10
+
+WORKDIR /app
+
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+RUN apt-get update && apt-get install -y google-chrome-stable
+
+COPY requirements.txt /app/requirements.txt
+RUN pip install -r requirements.txt
+COPY . /app
+CMD python main.py  
--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-import distro
-import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
-# Get the distribution name
-distname = distro.id()
+options = Options()
+options.add_argument('headless')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()), options=options)
+url = "https://www.forexfactory.com/calendar"
+driver.get(url)
 
-# Set the name of the Google Chrome binary based on the distribution
-if distname == "ubuntu" or distname == "debian":
-    chrome_binary = "google-chrome-stable"
-elif distname == "fedora" or distname == "centos" or distname == "rhel":
-    chrome_binary = "google-chrome"
-elif distname == "arch":
-    chrome_binary = "google-chrome-stable"
-elif distname == "opensuse":
-    chrome_binary = "google-chrome-stable"
-else:
-    print("Unsupported Linux distribution")
-    exit()
-
-# Check if Google Chrome is installed
 try:
-    subprocess.check_output(['which', chrome_binary])
-except subprocess.CalledProcessError:
-    print("Google Chrome is not installed")
-    exit()
+    # Wait for the tbody tag to appear on the webpage
+    tbody_locator = (By.TAG_NAME, 'tbody')
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located(tbody_locator))
 
-# Launch Google Chrome
-subprocess.Popen([chrome_binary])
+    # Get the page source and parse it with BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'html5lib')
 
-# Get the version of Google Chrome
-version_info = subprocess.check_output([chrome_binary, '--version']).decode().strip()
-print("Google Chrome version:", version_info.split()[-1])
+    # Find the tbody tag and print it
+    tbody = soup.find('tbody')
+    print(tbody)
+    break
+except Exception as e:
+    print(f"Error: {e}")
+    print("Could not find the tbody tag on the webpage.")
+
+driver.quit()
