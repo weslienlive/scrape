3ee790d5acf5c4799ceaece8ee8ec6979ab9ba05
weslienlive <weslienlive@gmail.com>
2023-03-09 13:55:35 +0200

    shortcut PT7.32

--- main.py
+++ main.py
@@ -53,8 +53,9 @@ try:
         entry_data["time"] = ', '.join([t.text.strip() for t in time])
 
         # get name
-        name = entry.find("span", {"class" : "calendar__event-title"})
-        entry_data["name"] = name.text.strip()       
+        name_td = entry.find("td", {"class": "calendar__cell calendar__event event"})
+        name_span = name_td.find("span", {"class": "calendar__event-title"})
+        entry_data["name"] = name_span.text.strip()      
 
 
 
